0046e543486e8e814b3a6f5b6e62e9f6c75c2ce5
Taein142 <xodlsdldy@naver.com>
2023-12-01 13:56:49 +0900

    if문 추가

--- src/ch03_scanner/Ex01_Scanner.java
+++ src/ch03_scanner/Ex01_Scanner.java
@@ -9,11 +9,28 @@ public class Ex01_Scanner {
 		// TODO Auto-generated method stub
 		// scanner 라는 이름의 Scanner 클래스 객체 선언
 		Scanner scanner = new Scanner(System.in);
-		// Scanner에 빨간 밑줄이 뜨면 마우스를 대고 import를 눌러주면 된다.
+		// Scanner 에 빨간 밑줄이 뜨면 마우스를 대고 import를 눌러주면 된다.
 		System.out.print("숫자를 입력하세요: ");
 		// 'ln'옵션은 줄을 바꿔버리는 옵션이다.
 		int num1 = scanner.nextInt();
 		System.out.println(num1);
+		
+		System.out.print("이름을 입력하세요: ");
+		String name = scanner.next();
+		System.out.println(name);
+		
+		scanner.nextLine();
+		//가끔 오류 있어서 이렇게 한 줄 줘야할 경우가 있음
+		
+		System.out.print("주소를 입력하세요: ");
+		String address = scanner.nextLine();
+		System.out.println(address);
+//		.next() 는 뛰어쓰기 전까지만 입력해주기 때문에
+//		.nextLine()을 사용해야 한다.
+		
+		
+		
+		
 	}
 
 }
