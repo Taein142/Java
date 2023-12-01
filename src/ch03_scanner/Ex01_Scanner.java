6a1ec5713ce8f148c2fc1f15410af46d53c852b0
Taein142 <xodlsdldy@naver.com>
2023-12-01 11:32:59 +0900

    check

--- src/ch03_scanner/Ex01_Scanner.java
+++ src/ch03_scanner/Ex01_Scanner.java
@@ -9,6 +9,7 @@ public class Ex01_Scanner {
 		// TODO Auto-generated method stub
 		// scanner 라는 이름의 Scanner 클래스 객체 선언
 		Scanner scanner = new Scanner(System.in);
+		// Scanner에 빨간 밑줄이 뜨면 마우스를 대고 import를 눌러주면 된다.
 		System.out.print("숫자를 입력하세요: ");
 		// 'ln'옵션은 줄을 바꿔버리는 옵션이다.
 		int num1 = scanner.nextInt();
