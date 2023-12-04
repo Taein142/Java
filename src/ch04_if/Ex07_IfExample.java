e94461d7d0679559074fe2401eba9b5942920557
Taein142 <xodlsdldy@naver.com>
2023-12-04 11:21:45 +0900

    if문 완료

--- src/ch04_if/Ex07_IfExample.java
+++ src/ch04_if/Ex07_IfExample.java
@@ -10,31 +10,28 @@ public class Ex07_IfExample {
 //		처리
 //		 - 1~3학년은 60점 이상이면 합격
 //		 - 4학년은 70점 이상이어야 합격
-		
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("학년을 입력해주세요 : ");
 		int year = scanner.nextInt();
 		if (year < 0 || year > 4) {
 			System.out.println("학년(year)을 다시 입력해주세요.(1~4까지)");
-		}else {
+		} else {
 			System.out.print("점수를 입력해주세요 : ");
 			int score = scanner.nextInt();
-			
-			if(year == 4) {
-				if(score >= 70){
+			if (year == 4) {
+				if (score >= 70) {
 					System.out.println("합격입니다.");
-				}else {
+				} else {
 					System.out.println("불합격입니다.");
 				}
-			}else {
-				if(score >= 60){
+			} else {
+				if (score >= 60) {
 					System.out.println("합격입니다.");
-				}else {
+				} else {
 					System.out.println("불합격입니다.");
 				}
 			}
 		}
-		
-		
 	}
 }
